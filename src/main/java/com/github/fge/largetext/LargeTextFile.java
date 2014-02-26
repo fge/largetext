290566e084084d8b4464379ec1aabbd61d1405f1
Francis Galiegue <fgaliegue@gmail.com>
2014-02-26 14:25:40 +0100

    LargeTextFile: change TODO about fixed length encodings
    It appears that a decoder's .{average,max}CharsPerByte() don't cut the mustard
    in order to detect fixed length encodings. Instead, an encoder's
    .{average,max}BytesPerChar() should be used.
    
    Strangely enough, the latter methods also return floats. Go figure.

--- src/main/java/com/github/fge/largetext/LargeTextFile.java
+++ src/main/java/com/github/fge/largetext/LargeTextFile.java
@@ -47,13 +47,14 @@ import static java.nio.channels.FileChannel.MapMode;
  * - deal with overly large files (ie, with more than Integer.MAX_VALUE chars)
  * - deal with small sizes
  * - implement .subsequence()
- * - optimize for character sets whose average chars per bytes is exactly 1
+ * - optimize for character sets whose average bytes per char and max bytes per
+ *   char are exactly the same
  *
  * FIXME: .length() is quite the nightmare; it requires to read the whole file
  * once. We cannot even optimize construction of 1-byte encodings because of
  * that! (or can we?)
  *
- * NOTE: if using .averageCharsPerByte(), remember to deal with an overflow
+ * NOTE: if using .averageBytesPerChar(), remember to deal with an overflow
  * error. Right now, the created CharBuffer has the same length as the mapping
  * size, which is always enough; this is of course untrue for variable length
  * encodings, the most prominent of them being UTF-8.
